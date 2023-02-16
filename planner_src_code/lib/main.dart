094c5a8c691d65779866fa2e6f921e6bbe7b80b9
Joseph Newman <josephnew900@gmail.com>
2023-02-15 17:33:12 -0800

    Update main.dart
    test

--- planner_src_code/lib/main.dart
+++ planner_src_code/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.red,
-      ),
+      ), 
       home: const MyHomePage(title: 'Vacation Planner'),
     );
   }
